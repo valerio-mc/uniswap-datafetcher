3a4a569e7293e53fc6593bcbef65c92f081d06f4
Valerio Maria Caccavella <valeriom.caccavella@gmail.com>
2022-03-23 22:44:32 +0100

    Fetching Historical Swap Feeds using getLogs() provider's function. poolInterface object to extract args data. Not sure how to store BigNumber values in a usable data form.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ethers, BigNumber } = require("ethers");
 const secrets = require('./secrets.json');
-const IUniswapV3Pool = require("@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json")
+const IUniswapV3Pool = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
 
 const provider = new ethers.providers.JsonRpcProvider(secrets.urlPolygon);
 const wallet = new ethers.Wallet(secrets.keeper);
@@ -8,20 +8,31 @@ const signer = wallet.connect(provider);
 
 // Polygon - wETH/USDC pool address
 const poolAddress = "0x45dda9cb7c25131df268515131f647d726f50608";
-// Loading pool's smart contract
-const PoolContract = new ethers.Contract(poolAddress, IUniswapV3Pool.abi, signer);
 
 async function main(){
     console.log("### Starting Uniswap-DataFetcher ###");
+    blockNumber = await provider.getBlockNumber();
+    poolInterface = new ethers.utils.Interface(IUniswapV3Pool.abi);
 
-    PoolContract.on("Swap", (sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick) => {
-        let swapPrice = new BigNumber.from(2).pow(192).mul(new BigNumber.from(10).pow(30)).div(sqrtPriceX96.pow(2));
-        swapPrice = ethers.utils.formatEther(swapPrice).toString();
-        console.log("A Swap occured @ price: ", swapPrice);
-        console.log("Amount token 0", ethers.utils.formatEther(amount0.mul(10**12)));
-        console.log("Amount token 1", ethers.utils.formatEther(amount1));
-    });
-    
+    let filter = {
+        address: poolAddress,
+        // 20'000 blocks gives approximately 5k events
+        fromBlock: blockNumber-100,
+        toBlock: 'latest',
+        topic: "Swap(address, address, int256, int256, uint160, uint128, int24)"
+    }
+
+    const logs = await provider.getLogs(filter);
+
+    /* for(i in logs){
+        
+
+        console.log(logs.length, " ", tx);
+    }*/
+
+
+    let out = poolInterface.parseLog(logs[0]);
+    console.log(out.args.liquidity.toNumber());
 
 }
 
