1b540290e3a6999fe9e370b583067b7264513ea6
Valerio Maria Caccavella <valeriom.caccavella@gmail.com>
2022-03-24 23:03:17 +0100

    Moved from halfdays to quarter days for response size limit. Added progress bar.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { ethers, BigNumber } = require("ethers");
 const secrets = require('./secrets.json');
 const IUniswapV3Pool = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
-const dfd = require("danfojs-node")
+const dfd = require("danfojs-node");
+const cliProgress = require('cli-progress');
+
 
 const provider = new ethers.providers.JsonRpcProvider(secrets.urlPolygon);
 const wallet = new ethers.Wallet(secrets.keeper);
@@ -14,19 +16,20 @@ async function main(){
     console.log("### Starting Uniswap-DataFetcher ###");
     let blockNumber = await provider.getBlockNumber();
     let poolInterface = new ethers.utils.Interface(IUniswapV3Pool.abi);
-    let halfdays = 1;
-    let windowSize = 20000;
+    let qdays = 120; // aka quarter of a day --> 120 qdays equal to 30 days
+    let windowSize = 10000;
     let rawDataFrame = [];
-    let header = ["blockNumber", "USDC", "wETH", "tick"]
+    let header = ["blockNumber", "USDC", "wETH", "tick"];
 
-    for(let step = halfdays; step > 0; step--){
-        console.log("Current block: ", blockNumber);
-        console.log("fromBlock: ", blockNumber - (step*windowSize));
-        console.log("toBlock: ", blockNumber - (step-1)*windowSize);
+    // create a new progress bar instance and use shades_classic theme
+    const pbar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
+    // start the progress bar with a total value equal to n° of halfdays and start value equal to 0
+    pbar.start(qdays, 0);
+    for(let step = qdays; step > 0; step--){
         let filter = {
             address: poolAddress,
-            // 20'000 blocks is roughly equivalent to 11h and gives approximately 5k events and is 
+            // 10'000 blocks is roughly equivalent to 5.5h and gives approximately 2.5k events 
             fromBlock: blockNumber - (step*windowSize),
             toBlock: blockNumber - (step-1)*windowSize,
             topic: "Swap(address, address, int256, int256, uint160, uint128, int24)"
@@ -43,7 +46,12 @@ async function main(){
                     swap.args.tick]);
             }
         }
+        // update the current value in your application..
+        pbar.update(1*(qdays - step));
     }
+
+    // stop the progress bar
+    pbar.stop();
 
     let df = new dfd.DataFrame(rawDataFrame, {columns: header})
     df.toCSV({ filePath: "wETH_USDC_pool.csv"});
